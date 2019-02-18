7c441b8680f2511d2a20a77ad74115b7571e1b38
AJODA-DESKTOP\Ajoda <ajodasattaur@gmail.com>
2019-02-18 01:22:07 -0500

    added docstring to boto_client module

--- wrappers/boto_client.py
+++ wrappers/boto_client.py
@@ -3,6 +3,16 @@ import boto3
 def boto_client(session_type, profile, region_name):
     '''
     Boto3 Session handler
+
+    Parameters:
+
+        session_type (service of aws to create a connection to)
+        profile (local aws profile to use to authenticate your connection)
+        region_name (aws region to connect to)
+
+    Returns:
+    
+        boto client object for use with the AWS api
     '''
     
     return boto3.Session(
