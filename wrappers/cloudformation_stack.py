c8c88fee21d82c1fcd14abaab82b61f72b07589d
AJODA-DESKTOP\Ajoda <ajodasattaur@gmail.com>
2019-02-18 12:18:07 -0500

    Single class file for all stack actions

---
+++ wrappers/cloudformation_stack.py
@@ -0,0 +1,34 @@
+# Module that will deploy the generated Cloudformation stack
+
+class CFStack:
+
+    def __init__(self, template_body, stack_name, cloudformation_client):
+
+        self.template_body              = template_body
+        self.stack_name                 = stack_name
+        self.cloudformation_client      = cloudformation_client
+
+    def deploy_stack(self):
+        '''
+        Uses the python generated Cloudformation valid yaml to deploy a new CF stack in the region you want.
+
+        Parameters:
+
+        template_body (valid Cloudformation yaml)
+        region (aws region to deploy into)
+        stack_name (name tag to give the stack)
+
+        Returns:
+            nothing
+        '''
+        
+        self.cloudformation_client.create_stack(
+                                            StackName       = self.stack_name,
+                                            TemplateBody    = self.template_body )
+        
+        print('---[Waiting for stack creation to complete')
+        waiter = self.cloudformation_client.get_waiter('stack_create_complete')
+        waiter.wait(StackName = self.stack_name)
+
+        
+
